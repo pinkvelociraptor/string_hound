ee99cedc1eb6ae41f0f1000cf5360f62590cb2b0
Noel Dellofano <noel@zendesk.com>
2012-03-05 15:13:50 -0800

    - Add default directory to hunt

--- lib/string_hound/tasks.rb
+++ lib/string_hound/tasks.rb
@@ -1,8 +1,8 @@
 require 'string_hound'
 
-desc "Given a directory, traverse through it and output all strings in all files"
+desc "Given a directory, traverse through it and output all strings in all files. Use current direcotry if one isn't given"
 task :hunt do
-  dir = ARGV.shift
+  dir = '.' if ARGV.count < 2 ? '.' : ARGV.pop
   hound = StringHound.new(dir)
   hound.hunt
 end
