8e6b6c0e66afa7961605543f3e8b8e206d63e599
Noel Dellofano <noel@zendesk.com>
2012-03-05 22:59:45 -0800

    -Add 'set leash' task to make the default search path settable - cleanup whitespaces in output - arg cleaning

--- lib/string_hound.rb
+++ lib/string_hound.rb
@@ -4,24 +4,27 @@ require 'nokogiri'
 ##
 # Given a directory, StringHound recursively searches the directory
 # heirarchy looking for any hardcoded strings.  When found, it prints them
-# to standard out in the form <filename>: <line>    <string value>
+# to standard out in the form:
+#   <filename>: <line>    <string value>
 #
-# This handles rather simple strings for now, not including
-# strings formed with CONTENT>> magic or multiline strings
+# If no directory is given, StringHound will search the default_leash dir.
+# This default behavior can be set via the set_leash rake task
 ##
 
 class StringHound
 
   attr_reader :view_file
+  class << self; attr_accessor :default_leash end
+  @default_leash = "app"
 
-  def initialize(dir)
-    @directory = dir
+  def initialize(dir = nil)
+    @directory = dir.present? ? dir : self.class.default_leash
     @prize = []
   end
 
   def hunt
     Find.find(@directory) do |f|
-      unless File.directory?(f)
+      unless FileTest.directory?(f)
         @file = f
         @view_file = ['.html', '.erb'].include?(File.extname(f))
         sniff
@@ -33,7 +36,11 @@ class StringHound
   def sniff
     File.readlines(@file).each_with_index do |l, line_num|
       match = taste(l)
-      match.each {|m| @prize << {:filename => @file, :line_number => line_num, :value => m}}
+      match.each do |m|
+        if m =~ /[\w]/
+          @prize << {:filename => @file, :line_number => line_num, :value => m}
+        end
+      end
     end
   end
 
--- lib/string_hound/tasks.rb
+++ lib/string_hound/tasks.rb
@@ -1,8 +1,18 @@
 require 'string_hound'
 
-desc "Given a directory, traverse through it and output all strings in all files. Use current direcotry if one isn't given"
+desc "Given a directory, traverse through it and output all strings in all files. Searches DEFAULT_DIR directory if none given"
 task :hunt do
-  dir = '.' if ARGV.count < 2 ? '.' : ARGV.pop
+  dir = ARGV.count < 2 ? nil : ARGV.pop
   hound = StringHound.new(dir)
   hound.hunt
+end
+
+desc "Change the default directory StringHound will hunt through if given no arguments"
+task :set_leash do
+  if ARGV.count < 2
+    puts "Incorrect number of arguments. Please give a directory name"
+  else
+    StringHound.default_leash = ARGV.pop
+    puts "Leash is now #{StringHound.default_leash}"
+  end
 end
